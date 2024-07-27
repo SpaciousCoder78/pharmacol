65faeecea553150dccdfb9016df9acd5ecbb3ec5
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-07-27 16:25:42 +0530

    using cdn for favicons

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const currentPage = ref('login'); // 'login' or 'signin'
 
 <template>
   <div>
-    <h1><img src="favicon.png" width="75" height="auto" loading="lazy">Pharmacol</h1>
+    <h1><img src="https://media.discordapp.net/attachments/721629168105685053/1266710421885161544/favicon.png?ex=66a62382&is=66a4d202&hm=4241727ef6b6b140999a29bb0170717872ec3ca9cd3959f31633e64d15b3d197&=&format=webp&quality=lossless&width=733&height=733" width="75" height="auto" loading="lazy">Pharmacol</h1>
     <button @click="currentPage = 'login'">Login</button>
     <button @click="currentPage = 'signin'">Sign In</button>
     <Login v-if="currentPage === 'login'"/>
