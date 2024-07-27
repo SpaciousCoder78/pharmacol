02d334366377075c4facec22ee66d6c897bf63c4
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-07-27 07:55:22 +0530

    deleted old files

--- src/main/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-package com.github.spaciouscoder78.pharmacol
-
-import menu
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val menu =  menu()
-}
