bd72f83359bba63def1c49974b37a42e1d54b38b
Shastha <shasthatyarla@gmail.com>
2024-07-28 15:51:05 +0530

    Testing
    all database related issues are to be tested here

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDZ1pFlIJtDLubOQqkXCctcpwU8F6cLxec",
+  authDomain: "pharmacy-billing-system.firebaseapp.com",
+  projectId: "pharmacy-billing-system",
+  storageBucket: "pharmacy-billing-system.appspot.com",
+  messagingSenderId: "452819719507",
+  appId: "1:452819719507:web:94de166e9069807c283da6",
+  measurementId: "G-FGM084WV3W"
 };
 
 // Initialize Firebase
@@ -13,4 +20,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export { auth };
+// Initialize Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export { auth, db };
